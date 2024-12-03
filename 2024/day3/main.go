f5a3b43306bc0b99da375a31f276252a9fd517ac
Kamil-Budzik <mixboksu@gmail.com>
2024-12-03 11:38:49 +0100

    [2024] Day 3 - Part 2

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func readFile() string {
-	file, err := os.Open("day3.test.txt")
+	file, err := os.Open("day3.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -35,6 +35,37 @@ func extractMulValues(input string) []string {
 	return matches
 }
 
+func extractInstructions(input string) []string {
+	// match mul(X,Y), do(), and don't()
+	regex := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)`)
+
+	matches := regex.FindAllString(input, -1)
+
+	return matches
+}
+
+func extractNumbersFromInstruction(v string, prefix string) (int, int) {
+	v = strings.TrimPrefix(v, prefix+"(")
+	v = strings.TrimSuffix(v, ")")
+	nums := strings.Split(v, ",")
+
+	if len(nums) != 2 {
+		fmt.Println("Numbers length should be 2")
+	}
+
+	leftNumber, err := strconv.Atoi(nums[0])
+	if err != nil {
+		fmt.Println("Failed to convert Left number", err)
+	}
+
+	rightNumber, _ := strconv.Atoi(nums[1])
+	if err != nil {
+		fmt.Println("Failed to convert right number", err)
+	}
+
+	return rightNumber, leftNumber
+}
+
 func part1() {
 	input := readFile()
 
@@ -43,24 +74,7 @@ func part1() {
 	total := 0
 
 	for _, v := range mulValues {
-		v = strings.TrimPrefix(v, "mul(")
-		v = strings.TrimSuffix(v, ")")
-		nums := strings.Split(v, ",")
-
-		if len(nums) != 2 {
-			fmt.Println("Numbers length should be 2")
-		}
-
-		leftNumber, err := strconv.Atoi(nums[0])
-		if err != nil {
-			fmt.Println("Failed to convert Left number", err)
-		}
-
-		rightNumber, _ := strconv.Atoi(nums[1])
-		if err != nil {
-			fmt.Println("Failed to convert right number", err)
-		}
-
+		leftNumber, rightNumber := extractNumbersFromInstruction(v, "mul")
 		total += leftNumber * rightNumber
 	}
 
@@ -68,6 +82,38 @@ func part1() {
 
 }
 
+func part2() {
+	input := readFile()
+
+	const enableInstruction = "do()"
+	const disableInstruction = "don't()"
+
+	instructions := extractInstructions(input)
+	isEnabled := true
+	total := 0
+
+	for _, ins := range instructions {
+
+		switch ins {
+		case enableInstruction:
+			isEnabled = true
+		case disableInstruction:
+			isEnabled = false
+		default:
+			leftNumber, rightNumber := extractNumbersFromInstruction(ins, "mul")
+
+			if isEnabled {
+				total += leftNumber * rightNumber
+			}
+		}
+
+	}
+
+	fmt.Println("Part 2 answer is", total)
+
+}
+
 func main() {
 	part1()
+	part2()
 }
