e32a9afb9476f3df7cafce347c576c0baf7033a6
Kamil-Budzik <mixboksu@gmail.com>
2024-12-03 11:48:58 +0100

    [2024] Day 3 - code improvement

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -18,14 +18,12 @@ func readFile() string {
 	scanner := bufio.NewScanner(file)
 	defer file.Close()
 
-	var input string
+	var input strings.Builder
 	for scanner.Scan() {
-		text := scanner.Text()
-
-		input += text
+		input.WriteString(scanner.Text())
 	}
 
-	return input
+	return input.String()
 }
 
 func extractMulValues(input string) []string {
@@ -36,34 +34,33 @@ func extractMulValues(input string) []string {
 }
 
 func extractInstructions(input string) []string {
-	// match mul(X,Y), do(), and don't()
+	// match mul(X,Y), do(), and don't(), where X and Y are 1-3 digit numbers
 	regex := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)`)
-
 	matches := regex.FindAllString(input, -1)
 
 	return matches
 }
 
-func extractNumbersFromInstruction(v string, prefix string) (int, int) {
+func extractNumbersFromInstruction(v string, prefix string) (int, int, error) {
 	v = strings.TrimPrefix(v, prefix+"(")
 	v = strings.TrimSuffix(v, ")")
 	nums := strings.Split(v, ",")
 
 	if len(nums) != 2 {
-		fmt.Println("Numbers length should be 2")
+		return 0, 0, fmt.Errorf("invalid instruction format: %s", v)
 	}
 
 	leftNumber, err := strconv.Atoi(nums[0])
 	if err != nil {
-		fmt.Println("Failed to convert Left number", err)
+		return 0, 0, fmt.Errorf("failed to convert left number: %s", nums[0])
 	}
 
-	rightNumber, _ := strconv.Atoi(nums[1])
+	rightNumber, err := strconv.Atoi(nums[1])
 	if err != nil {
-		fmt.Println("Failed to convert right number", err)
+		return 0, 0, fmt.Errorf("failed to convert right number: %s", nums[1])
 	}
 
-	return rightNumber, leftNumber
+	return leftNumber, rightNumber, nil
 }
 
 func part1() {
@@ -74,7 +71,11 @@ func part1() {
 	total := 0
 
 	for _, v := range mulValues {
-		leftNumber, rightNumber := extractNumbersFromInstruction(v, "mul")
+		leftNumber, rightNumber, err := extractNumbersFromInstruction(v, "mul")
+		if err != nil {
+			fmt.Println("Error extracting numbers", err)
+			continue
+		}
 		total += leftNumber * rightNumber
 	}
 
@@ -100,7 +101,12 @@ func part2() {
 		case disableInstruction:
 			isEnabled = false
 		default:
-			leftNumber, rightNumber := extractNumbersFromInstruction(ins, "mul")
+			leftNumber, rightNumber, err := extractNumbersFromInstruction(ins, "mul")
+
+			if err != nil {
+				fmt.Println("Skipping invalid instruction", err)
+				continue
+			}
 
 			if isEnabled {
 				total += leftNumber * rightNumber
