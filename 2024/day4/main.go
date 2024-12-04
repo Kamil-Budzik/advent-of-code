b5b789dbec7d919b94202c546db14facbcdae1c3
Kamil-Budzik <mixboksu@gmail.com>
2024-12-04 11:56:58 +0100

    [2024] Day4 - Part 1

--- 2024/day4/main.go
+++ 2024/day4/main.go
@@ -6,7 +6,26 @@ import (
 	"os"
 )
 
-var directions = [][2]int{
+func readFile() [][]rune {
+	file, err := os.Open("day4.test.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+
+	var input [][]rune
+	for scanner.Scan() {
+		text := scanner.Text()
+		input = append(input, []rune(text))
+	}
+
+	return input
+
+}
+
+var directions = [8][2]int{
 	{-1, 0},
 	{1, 0},
 	{0, -1},
@@ -17,55 +36,46 @@ var directions = [][2]int{
 	{1, 1},
 }
 
-func readFile() []string {
-	file, err := os.Open("day4.test.txt")
-	if err != nil {
-		panic(err)
-	}
-
-	scanner := bufio.NewScanner(file)
-	defer file.Close()
-
-	var input []string
-	for scanner.Scan() {
-		text := scanner.Text()
-		input = append(input, text)
-	}
-
-	return input
-
-}
-
 func checkBounds(rows, cols, x, y int) bool {
 	return x >= 0 && x < rows && y >= 0 && y < cols
 }
 
-func part1() {
-	grid := readFile()
-	for _, row := range grid {
-		fmt.Println(row)
-	}
-
+func searchWord(grid [][]rune, word string) int {
 	rows := len(grid)
 	cols := len(grid[0])
 
-	for x, row := range grid {
-		for y, col := range row {
-			if grid[x][y] != byte('K') {
-				continue
-			}
-			_ = col
-			for _, d := range directions {
-				newX := x + d[0]
-				newY := y + d[1]
+	matches := 0
 
-				if checkBounds(rows, cols, newX, newY) {
-					fmt.Printf("Value at (%d, %d): %s\n", newX, newY, string(grid[newX][newY]))
+	for x, row := range grid {
+		for y := range row {
+			for _, d := range directions {
+				match := true
+
+				for i := 0; i < len(word); i++ {
+					newX := x + i*d[0]
+					newY := y + i*d[1]
+
+					if !checkBounds(rows, cols, newX, newY) || grid[newX][newY] != rune(word[i]) {
+						match = false
+						break
+					}
+				}
+
+				if match {
+					matches++
 				}
 			}
 		}
 	}
 
+	return matches
+}
+
+func part1() {
+	grid := readFile()
+
+	matches := searchWord(grid, "XMAS")
+	fmt.Println("Part 1 answer:", matches)
 }
 
 func main() {
