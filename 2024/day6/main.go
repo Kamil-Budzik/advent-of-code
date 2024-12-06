449d7b51a58ae3c50e0ecc677add8e2ed40b93db
Kamil-Budzik <mixboksu@gmail.com>
2024-12-06 12:23:01 +0100

    [2024] Day 6 - Part 1

---
+++ 2024/day6/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Matrix [][]rune
+
+const TOP = '^'
+const RIGHT = '>'
+const BOTTOM = 'v'
+const LEFT = '<'
+
+func readFile() (Matrix, int, int, rune) {
+	file, err := os.Open("day6.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+
+	var matrix Matrix
+	var startX, startY int
+	var startingDirection rune
+
+	for x := 0; scanner.Scan(); x++ {
+		var line []rune
+
+		for y, char := range scanner.Text() {
+			line = append(line, char)
+
+			if char == TOP || char == RIGHT || char == BOTTOM || char == LEFT {
+				startX, startY, startingDirection = x, y, char
+			}
+		}
+
+		matrix = append(matrix, line)
+	}
+
+	return matrix, startX, startY, startingDirection
+
+}
+
+func didEscape(x, y, rows, cols int) bool {
+	return x < 0 || x >= rows || y < 0 || y >= cols
+}
+
+func getNewDirection(currentDirection rune) rune {
+	switch currentDirection {
+	case TOP:
+		return RIGHT
+	case RIGHT:
+		return BOTTOM
+	case BOTTOM:
+		return LEFT
+	default: // LEFT case
+		return TOP
+	}
+}
+
+func didEncounterObstacle(nextPosition rune) bool {
+	return nextPosition == '#'
+}
+
+func generateGuardPath(matrix Matrix, x int, y int, direction rune) Matrix {
+	var newDirection rune = direction
+	newX, newY := x, y
+
+	switch direction {
+	case TOP:
+		newX = x - 1
+	case RIGHT:
+		newY = y + 1
+	case BOTTOM:
+		newX = x + 1
+	case LEFT:
+		newY = y - 1
+	}
+
+	if didEscape(newX, newY, len(matrix), len(matrix[0])) {
+		matrix[x][y] = 'X'
+		return matrix
+	}
+
+	if didEncounterObstacle(matrix[newX][newY]) {
+		newDirection = getNewDirection(direction)
+		matrix[x][y] = 'X'
+		return generateGuardPath(matrix, x, y, newDirection)
+	}
+
+	matrix[newX][newY] = direction
+	matrix[x][y] = 'X'
+	return generateGuardPath(matrix, newX, newY, newDirection)
+}
+
+func countDistincsPositions(matrix Matrix) int {
+	total := 0
+	for _, rows := range matrix {
+		for _, y := range rows {
+			if y == 'X' {
+				total++
+			}
+		}
+	}
+
+	return total
+}
+
+func part1() {
+	matrix, startX, startY, direction := readFile()
+
+	guardPath := generateGuardPath(matrix, startX, startY, direction)
+
+	fmt.Println("Answer to part 1:", countDistincsPositions(guardPath))
+
+}
+
+func main() {
+	part1()
+}
