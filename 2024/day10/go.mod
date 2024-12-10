bcbaa370b5f4bdc3f26687b57d41a83a6a5bc72a
Kamil-Budzik <mixboksu@gmail.com>
2024-12-10 15:04:26 +0100

    [2024] Day 10 - Part 1

---
+++ 2024/day10/go.mod
@@ -0,0 +1,3 @@
+module advent-of-code.com/day10
+
+go 1.22.4
---
+++ 2024/day10/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Matrix [][]int
+
+func readFile() Matrix {
+	file, err := os.Open("day10.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+	var grid Matrix
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		line := strings.TrimSpace(text)
+
+		var row []int
+
+		for _, el := range line {
+			num, _ := strconv.Atoi(string(el))
+
+			row = append(row, num)
+		}
+
+		grid = append(grid, row)
+	}
+
+	return grid
+
+}
+
+func isInBounds(x, y, W, H int) bool {
+	return x >= 0 && x < H && y >= 0 && y < W
+}
+
+var directions = [8][2]int{
+	{-1, 0},
+	{1, 0},
+	{0, -1},
+	{0, 1},
+}
+
+// It will return the grid position of 9, if the trailhead is correct
+func createTrailHead(grid Matrix, x, y int, visited map[[2]int]bool) [][]int {
+	if grid[x][y] == 9 {
+		return [][]int{{x, y}}
+	}
+
+	visited[[2]int{x, y}] = true
+	reachableNines := [][]int{}
+
+	for _, dir := range directions {
+		newX, newY := x+dir[0], y+dir[1]
+
+		if isInBounds(newX, newY, len(grid[0]), len(grid)) &&
+			!visited[[2]int{newX, newY}] &&
+			grid[newX][newY]-grid[x][y] == 1 {
+
+			result := createTrailHead(grid, newX, newY, visited)
+			reachableNines = append(reachableNines, result...)
+		}
+	}
+
+	return reachableNines
+}
+
+func part1() {
+	grid := readFile()
+
+	H := len(grid)
+	W := len(grid[0])
+	total := 0
+
+	for x := 0; x < H; x++ {
+		for y := 0; y < W; y++ {
+			if grid[x][y] == 0 {
+				visited := make(map[[2]int]bool)
+				reachableNines := createTrailHead(grid, x, y, visited)
+
+				uniqueNines := make(map[[2]int]bool)
+				for _, pos := range reachableNines {
+					uniqueNines[[2]int{pos[0], pos[1]}] = true
+				}
+
+				score := len(uniqueNines)
+				total += score
+
+			}
+		}
+	}
+
+	fmt.Println("Answer to part 1", total)
+}
+
+func main() {
+	part1()
+}
