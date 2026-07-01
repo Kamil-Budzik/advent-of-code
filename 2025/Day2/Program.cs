12a6879d95d82357d808e835273a8534714fe639
Kamil-Budzik <mixboksu@gmail.com>
2026-07-01 11:21:09 +0200

    2025: Day 2: Part 2

--- 2025/Day2/Program.cs
+++ 2025/Day2/Program.cs
@@ -1,7 +1,8 @@
 ﻿var line = File.ReadAllText("./day2.txt").Trim();
 var parts = line.Split(',');
 
-long total = 0;
+long totalPart1 = 0;
+long totalPart2 = 0;
 
 foreach (var part in parts)
 {
@@ -11,15 +12,40 @@ foreach (var part in parts)
 
     for (long id = first; id <= last; id++)
     {
-        if (isInvalid(id.ToString())) total += id;
+        if (isInvalidPart1(id.ToString())) totalPart1 += id;
+        if (isInvalidPart2(id.ToString())) totalPart2 += id;
     }
 }
 
-Console.WriteLine(total);
+Console.WriteLine(totalPart1);
+Console.WriteLine(totalPart2);
 
-bool isInvalid(string id)
+bool isInvalidPart1(string id)
 {
     if (id.Length % 2 != 0) return false;
     int half = id.Length / 2;
     return id[..half] == id[half..];
+}
+
+bool isInvalidPart2(string id)
+{
+    for (int unitLength = 1; unitLength <= id.Length / 2; unitLength++)
+    {
+        if (id.Length % unitLength != 0) continue;
+
+        string unit = id[..unitLength];
+        bool allMatch = true;
+
+        for (int i = unitLength; i < id.Length; i += unitLength)
+        {
+            if (id[i..(i + unitLength)] != unit)
+            {
+                allMatch = false;
+                break;
+            }
+        }
+
+        if (allMatch) return true;
+    }
+    return false;
 }
